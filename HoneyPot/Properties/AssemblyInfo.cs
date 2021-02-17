ba339624b359e62ac121dc5784d9a43c7349f903
json burn <vbnmnbvbnm1234@gmail.com>
2021-02-17 10:34:05 +0800

    Bump version

--- HoneyPot/Properties/AssemblyInfo.cs
+++ HoneyPot/Properties/AssemblyInfo.cs
@@ -6,15 +6,15 @@ using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Permissions;
 
-[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: AssemblyVersion("1.4.3.0")]
 [assembly: AssemblyTitle("HoneyPot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HoneyPot")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b29b3769-c0f6-4ae3-b802-4f3de4810061")]
-[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.4.3.0")]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
