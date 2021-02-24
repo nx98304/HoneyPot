b77dc1e9d8174602a68e796eb4c26451cf78c90f
json burn <vbnmnbvbnm1234@gmail.com>
2021-02-24 11:37:40 +0800

    added Harmony Patch to AttachBoneWeight.SetupRenderer, making sure the rootBone has default value.

---
+++ HoneyPot/AttachBoneWeight_SetupRenderer_Patch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using HarmonyLib;
+
+namespace ClassLibrary4
+{
+    [HarmonyPatch(typeof(AttachBoneWeight), "SetupRenderer", 
+        new Type[] { typeof(Dictionary<string, Transform>), typeof(SkinnedMeshRenderer) }
+    )]
+    public class AttachBoneWeight_SetupRenderer_Patch
+    {
+        public static void Prefix(Dictionary<string, Transform> bones, ref SkinnedMeshRenderer renderer)
+        {
+            if(renderer.rootBone == null )
+            {
+                Console.WriteLine("This SkinnedMeshRenderer doesn't have a rootBone to begin with. Trying cf_J_Root...");
+                renderer.rootBone = bones["cf_J_Root"];
+                if(renderer.rootBone == null )
+                {
+                    Console.WriteLine(" -- failed. Trying cm_J_Root...");
+                    renderer.rootBone = bones["cm_J_Root"];
+                    if(renderer.rootBone == null )
+                    {
+                        Console.WriteLine(" SOMETHING IS VERY WRONG HERE!!!! ");
+                    }
+                }
+            }
+        }
+    }
+}
