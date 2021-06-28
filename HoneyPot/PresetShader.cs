0919e9b8e7a29e2307ab14283a8bf7e59a6e5db7
json burn <vbnmnbvbnm1234@gmail.com>
2021-06-28 21:01:37 +0800

    code cleanup

--- HoneyPot/PresetShader.cs
+++ HoneyPot/PresetShader.cs
@@ -6,25 +6,13 @@ namespace ClassLibrary4
 	// Token: 0x0200000D RID: 13
 	internal class PresetShader
 	{
-		// Token: 0x0400002C RID: 44
-		public string assetBundleName;
+		//public string assetBundleName;
+		//public string assetName;
+		//public int rendererIdx;
+		//public int materialIdx;
+		//public string doCopyMaterial;
 
-		// Token: 0x0400002D RID: 45
-		public string assetName;
-
-		// Token: 0x0400002E RID: 46
-		public int rendererIdx;
-
-		// Token: 0x0400002F RID: 47
-		public int materialIdx;
-
-		// Token: 0x04000030 RID: 48
-		public string doCopyMaterial;
-
-		// Token: 0x04000031 RID: 49
 		public Shader shader;
-
-		// Token: 0x04000032 RID: 50
-//		public Material material;
-	}
+        //public Material material;
+    }
 }
