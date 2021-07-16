35de160cb1e24e8d81445de0302ef922dfc2244a
json burn <vbnmnbvbnm1234@gmail.com>
2021-07-17 02:03:07 +0800

    Transpiler stuff into WearInstantiate so that stupid clothing without cf_N_O_root can work.

--- HoneyPot/Wears_UpdateShow_Patches.cs
+++ HoneyPot/Wears_UpdateShow_Patches.cs
@@ -1,12 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Linq;
 using UnityEngine;
 using HarmonyLib;
 using Character;
 
 namespace ClassLibrary4
 {
+    //Note: This patch is for removing the cm/cf_N_O_root transform check within WearInstantiate
+    //      Because I have to make this work with HS1 clothings that doesn't conform to the HS1/PH shared clothing spec
+    //      Additional checks and animator transform tree structure manipulation added within 
+    //      AttachBoneWeight.Attach() with yet another Prefix patch.
+    [HarmonyPatch(typeof(Wears), "WearInstantiate")]
+    public class Wears_WearInstantiate_Patch
+    {
+        private static void create_N_O_root_if_not_found(ref Transform iter, Transform prefab_anim_root)
+        {
+            if (iter != null) return; //means N_O_root is already found prior to this, skip it. 
+            bool female = Transform_Utility.FindTransform(prefab_anim_root, "cf_J_Root") != null ? true : false;
+            Transform iter_from_anim_root = null;
+            Transform parent_to_N_O_root = null;
+            List<Transform> children_to_N_O_root = new List<Transform>();
+            Queue<Transform> transform_BFS_queue = new Queue<Transform>();
+            for (int i = 0; i < prefab_anim_root.childCount; i++)
+                transform_BFS_queue.Enqueue(prefab_anim_root.GetChild(i));
+            while (transform_BFS_queue.Count > 0)
+            {
+                iter_from_anim_root = transform_BFS_queue.Dequeue();
+                if (iter_from_anim_root.name.IndexOf("N_") == 0)
+                {
+                    GameObject new_N_O_root = new GameObject( female ? "cf_N_O_root" : "cm_N_O_root" );
+                    parent_to_N_O_root = iter_from_anim_root.parent;
+                    
+                    for (int i = 0; i < parent_to_N_O_root.childCount; i++)
+                        children_to_N_O_root.Add(parent_to_N_O_root.GetChild(i));
+
+                    for (int i = 0; i < children_to_N_O_root.Count; i++)
+                        children_to_N_O_root[i].SetParent(new_N_O_root.transform);
+
+                    new_N_O_root.transform.SetParent(parent_to_N_O_root);
+                    iter = new_N_O_root.transform;
+                    break;
+                }
+                for (int i = 0; i < iter_from_anim_root.childCount; i++)
+                    transform_BFS_queue.Enqueue(iter_from_anim_root.GetChild(i));
+            }
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            FieldInfo fi1  = typeof(Wears).GetField("baseBoneRoot", BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo mi1 = typeof(AttachBoneWeight).GetMethod("Attach", BindingFlags.Public | BindingFlags.Static);
+            MethodInfo mi2 = typeof(Wears).GetMethod("ReAttachDynamicBone"); // ??? I can't specify BindingFlags here???
+            var codes = new List<CodeInstruction>(instructions);
+            int meshRootConditionStart_index = -1;
+            int meshRootConditionEnd_index = -1;
+            int elseBlockStart_index = -1;
+            int elseBlockEnd_index = -1;
+            for (int i = 0; i < codes.Count - 3; i++)
+            {
+                if (meshRootConditionStart_index < 0 &&
+                    codes[i].opcode == OpCodes.Ldloc_3)
+                {
+                    meshRootConditionStart_index = i;
+                }
+                else if(meshRootConditionEnd_index < 0 &&
+                        codes[i].opcode == OpCodes.Brfalse &&
+                        codes[i+2].opcode == OpCodes.Ldfld && 
+                        codes[i+2].operand == fi1)
+                {
+                    meshRootConditionEnd_index = i;
+                }
+                else if(elseBlockStart_index < 0 && 
+                        codes[i].opcode == OpCodes.Br && 
+                        codes[i-1].operand == mi1 )
+                {
+                    elseBlockStart_index = i;
+                }
+                else if(elseBlockEnd_index < 0 &&
+                        codes[i].opcode == OpCodes.Call && 
+                        codes[i+3].operand == mi2)
+                {
+                    elseBlockEnd_index = i;
+                }
+            }
+            for (int i = meshRootConditionStart_index; i <= meshRootConditionEnd_index; i++)
+                codes[i].opcode = OpCodes.Nop;
+            for (int i = elseBlockStart_index; i <= elseBlockEnd_index; i++)
+                codes[i].opcode = OpCodes.Nop;
+            //Note: So I don't know why -- removing the instructions by deleting them will break the program
+            //      while Nop works. Apparently I must have deleted some Br jump label targets with it
+            //      but I can't figure out what or where when looking at the IL code. Anyhow, Nop works,
+            //      that's what counts.
+
+            //Note: What I want to do here instead of the original code: 
+            //      Transform transform = Transform_Utility.FindTransform(gameObject.transform, this.wear_meshRootName);
+            //      create_N_O_root_if_not_found((ref)transform, gameObject.transform);
+            //      AttachBoneWeight.Attach(this.baseBoneRoot.gameObject, transform.gameObject, true);
+            codes[meshRootConditionStart_index]   = new CodeInstruction(OpCodes.Ldloca, 3); //loc3 is local var transform
+            codes[meshRootConditionStart_index+1] = new CodeInstruction(OpCodes.Ldloc_2);   //loc2 is local var gameObject
+            codes[meshRootConditionStart_index+2] = new CodeInstruction(OpCodes.Callvirt, 
+                typeof(GameObject).GetMethod("get_transform")); //gameObject.transform
+            codes[meshRootConditionStart_index+3] = new CodeInstruction(OpCodes.Call, 
+                typeof(Wears_WearInstantiate_Patch).GetMethod(nameof(create_N_O_root_if_not_found), BindingFlags.NonPublic | BindingFlags.Static));
+            return codes.AsEnumerable();
+        }
+    }
+
     [HarmonyPatch(typeof(Wears), "ReAttachDynamicBone", 
         new Type[] { typeof(GameObject) }
     )]
