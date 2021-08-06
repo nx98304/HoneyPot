449b0b8aacce5089e85d9362e1f5356fb47455d7
json burn <vbnmnbvbnm1234@gmail.com>
2021-08-06 13:11:47 +0800

    Due to MoreAccessories's different Accessory buttons initialization process, I cannot use parent tree to decide if they are under the right category. At least using the name works...

--- HoneyPot/EditMode_CreateColorChangeButton_Patch.cs
+++ HoneyPot/EditMode_CreateColorChangeButton_Patch.cs
@@ -14,12 +14,22 @@ namespace ClassLibrary4
         })]
     public class EditMode_CreateColorChangeButton_Patch
     {
+        public static bool allow_alpha = false;
         public static void Prefix(GameObject parent, ref bool hasAlpha)
-        {      // Top          > Main > Wear                    Slot01          > Main > Accessory
-            if (parent.transform.parent.parent.name == "Wear" || parent.transform.parent.parent.name == "Accessory")
+        {
+            if (allow_alpha && is_part_of_wear_or_acce(parent) )
             {
                 hasAlpha = true;
             }
+        }
+
+        private static bool is_part_of_wear_or_acce(GameObject o)
+        {
+            if (o.name == "Tops"  || o.name == "Bottoms" || o.name == "Bra"   || o.name == "Shorts" ||
+                o.name == "Glove" || o.name == "Panst"   || o.name == "Socks" || o.name == "Shoes"  ||
+                o.name.IndexOf("Swim") >= 0 || o.name.IndexOf("Slot") >= 0)
+                return true;
+            return false;
         }
     }
 }
